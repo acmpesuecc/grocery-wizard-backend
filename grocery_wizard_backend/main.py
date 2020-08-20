62496d3b6ecd88352be9190d9a421e10f5ef688f
Srujan <srujan.deshpande@gmail.com>
2020-08-20 23:28:26 +0530

    Header in request

--- grocery_wizard_backend/main.py
+++ grocery_wizard_backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from pydantic import BaseModel
 from typing import Optional
 
@@ -28,6 +28,6 @@ async def read_user(username: str):
     return {"id":username}
 
 @app.post("/users")
-async def create_user(user:User):
+async def create_user(user:User, user_id: str = Header(None)):
     user_dict = user.dict()
-    return user
+    return user_id
