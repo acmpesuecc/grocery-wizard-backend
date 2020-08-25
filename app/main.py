b4e111abb11f4665d34d62f0b93a4c6e21394bbb
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-08-25 15:58:43 +0530

    Travis CI Docker CD

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!"}
 
 @app.get("/users/{username}")
 async def read_user(username: str):
